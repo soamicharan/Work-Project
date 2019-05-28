c88118920ba742904fda2ba82d61bc967e51640f
Soami Charan <charansoami@gmail.com>
2019-05-28 19:57:38 +0530

    Update MyJavaScript.js

--- Work Project 1/MyJavaScript.js
+++ Work Project 1/MyJavaScript.js
@@ -1,7 +1,7 @@
 function changeBGcolor(element){
-var r=Math.floor(Math.random()*256);
-var g=Math.floor(Math.random()*256);
-var b=Math.floor(Math.random()*256);
-var col='rgb('+r+','+g+','+b+')';
-element.style.backgroundColor=col;
+  var r=Math.floor(Math.random()*256);
+  var g=Math.floor(Math.random()*256);
+  var b=Math.floor(Math.random()*256);
+  var col='rgb('+r+','+g+','+b+')';
+  element.style.backgroundColor=col;
 }
